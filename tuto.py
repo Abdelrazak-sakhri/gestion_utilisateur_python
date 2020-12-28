f1b589d9c88b11c1da325d4685513e7c6db10245
Abdel <abdelrazak.sakhri@gmail.com>
2020-12-28 01:11:32 +0100

    ajout d'un texte a 200px du haut de la page

--- tuto.py
+++ tuto.py
@@ -7,3 +7,9 @@ import webbrowser
 tuto = tkinter.Tk()
 tuto.title("Initiation TKinter")
 tuto.geometry("1280x620")
+mainmenu = tkinter.Menu(tuto)
+
+#Label
+l1 = Label(text = "Tuto release beta", font="ARIAL", bg="yellow")
+l1.pack(pady = 200)
+
