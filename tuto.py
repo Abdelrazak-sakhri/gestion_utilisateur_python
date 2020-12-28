72064267e818eb57da01ed6b037735426f748341
Abdel <abdelrazak.sakhri@gmail.com>
2020-12-28 01:14:29 +0100

    ajout de 3 fonctions open -  exit -  textfunny

--- tuto.py
+++ tuto.py
@@ -13,3 +13,14 @@ mainmenu = tkinter.Menu(tuto)
 l1 = Label(text = "Tuto release beta", font="ARIAL", bg="yellow")
 l1.pack(pady = 200)
 
+#Def
+def open():
+    webbrowser.open_new("https://www.youtube.com/watch?v=8nYUFJIuIpg")
+
+def exit():
+    quit()
+
+def textfunny():
+    l1 = Label(text = "Tuto initiation Tkinter")
+    l1.pack()
+
